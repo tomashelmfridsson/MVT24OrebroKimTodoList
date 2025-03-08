6d99b9586da20a8d0d282a58549ae2b8a0b1ebaa
Tomas Helmfridsson <tomas.helmfridsson@addq.se>
2025-03-08 17:24:32 +0100

    Add files via upload

---
+++ src/main/java/ToDoList/Todolist/todo/Task.java
@@ -0,0 +1,31 @@
+package ToDoList.Todolist.todo;
+
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void complete() {
+        isCompleted = true;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public String getDescription() {
+        String checked = "[ ] ";
+        if(isCompleted) {
+            checked = "[X] ";
+        }
+        return checked + description;
+    }
+
+    public void markAsNotCompleted() {
+        this.isCompleted = false;
+    }
+}
